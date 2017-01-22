eca5a9e5b61866626cb6c3f8f2ab456abddf2e14
Christophe Arguel <christophe.arguel@free.fr>
2017-01-22 21:55:21 +0100

    Replace windows_registry (no longer supported by up-to-date windows cookbook) by registry_key.

--- recipes/default.rb
+++ recipes/default.rb
@@ -37,78 +37,125 @@ else
   tile_image = ""
 end
 
-windows_registry pGina3 do
-  values(
-    "Motd" => node[:pgina][:motd],
-    "TileImage" => tile_image
-  )
-  type :string
+registry_key pGina3 do
+  values [
+    {
+      :name => "Motd",
+      :type => :string,
+      :data => node['pgina']['motd']
+    },
+    {
+      :name => "TileImage",
+      :type => :string,
+      :data => tile_image
+    }
+  ]
   action :create
 end
 
-# Disabling the password provider allows pGina to control RDP logons by default
-windows_registry pGina3 do
-  values "CredentialProviderFilters" => [ "{6f45dc1e-5384-457a-bc13-2cd81b0d28ed}\t15" ]
-  type :multi_string
+
+registry_key pGina3 do
+  values [
+    {
+      :name => "CredentialProviderFilters",
+      :type => :multi_string,
+      :data => [ "{6f45dc1e-5384-457a-bc13-2cd81b0d28ed}\t15" ]
+    }
+  ]
   only_if { node[:pgina][:disable_default_provider] }
   action :create
 end
 
 # LDAP Plugins
 #--------------
-windows_registry ldap_plugin do
-  values(
-    'LdapPort'    => node[:pgina][:ldap][:port],  
-    'LdapTimeout' => node[:pgina][:ldap][:timeout]
-  )
-  type :dword
-  action :create
-end
+registry_key ldap_plugin do
+  values [
+    {
+      :name => "LdapPort",
+      :type => :dword,
+      :data => node[:pgina][:ldap][:port]
+    },
+    {
+      :name => "LdapTimeout",
+      :type => :dword,
+      :data => node[:pgina][:ldap][:timeout]
+    },
+    {
+      :name => "DnPattern",
+      :type => :string,
+      :data => node[:pgina][:ldap][:user_dn_pattern]
+    },
+    {
+      :name => "UseSsl",
+      :type => :string,
+      :data => node[:pgina][:ldap][:ssl_enabled]
+    },
+    {
+      :name => "UseTls",
+      :type => :string,
+      :data => node[:pgina][:ldap][:tls_enabled]
+    },
+    {
+      :name => "RequireCert",
+      :type => :string,
+      :data => node[:pgina][:ldap][:require_cert]
+    },
+    {
+      :name => "ServerCertFile",
+      :type => :string,
+      :data => node[:pgina][:ldap][:require_cert]
+    },
+    {
+      :name => "UseAuthBindForAuthzAndGateway",
+      :type => :string,
+      :data => node[:pgina][:ldap][:bind_with_user_credentials]
+    },
+    {
+      :name => "SearchDN",
+      :type => :string,
+      :data => node[:pgina][:ldap][:search_dn]
+    },
+    {
+      :name => "DoSearch",
+      :type => :string,
+      :data => node[:pgina][:ldap][:search_enabled]
+    },
+    {
+      :name => "SearchFilter",
+      :type => :string,
+      :data => node[:pgina][:ldap][:search_filter]
+    },
 
-windows_registry ldap_plugin do
-  values(
-    'DnPattern'   => node[:pgina][:ldap][:user_dn_pattern],
-    'UseSsl'      => node[:pgina][:ldap][:ssl_enabled],
-    'UseTls'      => node[:pgina][:ldap][:tls_enabled],
-    'RequireCert'      => node[:pgina][:ldap][:require_cert],
-    'ServerCertFile'      => node[:pgina][:ldap][:require_cert],
-    'UseAuthBindForAuthzAndGateway' => node[:pgina][:ldap][:bind_with_user_credentials],
-    'SearchDN'    => node[:pgina][:ldap][:search_dn],
-    'DoSearch'          => node[:pgina][:ldap][:search_enabled],
-    'SearchFilter'      => node[:pgina][:ldap][:search_filter]
-  )
-  type :string
-  action :create
-end
-
-# registry_key ldap_plugin do
-#   values [
-#     {
-#       :name => 'SearchPW',
-#       :type => :string
-#     }
-#   ]
-#   action :delete
-# end
-
-windows_registry ldap_plugin do
-  values(
-    'SearchContexts' => node[:pgina][:ldap][:search_contexts],
-    'LdapHost'       => node[:pgina][:ldap][:hosts]
-  )
-  type :multi_string
+    {
+      :name => "SearchContexts",
+      :type => :multi_string,
+      :data => node[:pgina][:ldap][:search_contexts]
+    },
+    {
+      :name => "LdapHost",
+      :type => :multi_string,
+      :data => node[:pgina][:ldap][:hosts]
+    }
+  ]
   action :create
 end
 
 # The plugin state is a binary flag, this says all or nothing
 ldap_plugin_state = node[:pgina][:ldap][:enabled] ? 46 : 0
 
-windows_registry pGina3 do
-  values(
-    "0f52390b-c781-43ae-bd62-553c77fa4cf7" => ldap_plugin_state,
-    "12FA152D-A2E3-4C8D-9535-5DCD49DFCB6D" => 8                       # Enable local machine for gateway
-  )
-  type :dword
+registry_key pGina3 do
+  values [ 
+    {
+      :name => "0f52390b-c781-43ae-bd62-553c77fa4cf7",
+      :type => :dword,
+      :data => ldap_plugin_state
+    },
+    {
+      :name => "12FA152D-A2E3-4C8D-9535-5DCD49DFCB6D",
+      :type => :dword,
+      :data => 8
+    }
+  ]
   action :create
 end
 
@@ -116,20 +163,33 @@ end
 # Authorisation
 
 # If we require the server than we need it to be up and for authentication to succeed
-windows_registry ldap_plugin do 
-  values(
-    "AuthzAllowOnError" => node[:pgina][:ldap][:require_server] ? "False" : "True",
-    "AuthzRequireAuth" => node[:pgina][:ldap][:require_server] ? "True" : "False"
-  )
-  type :string
+registry_key ldap_plugin do
+  values [ 
+    {
+      :name => "AuthzAllowOnError",
+      :type => :string,
+      :data => node[:pgina][:ldap][:require_server] ? "False" : "True"
+    },
+    {
+      :name => "AuthzRequireAuth",
+      :type => :string,
+      :data => node[:pgina][:ldap][:require_server] ? "True" : "False"
+    }
+  ]
   action :create
+
 end
 
 authz_groups = node[:pgina][:ldap][:require_groups]
 authz_rules = ldap_authz_rules( authz_groups )
-windows_registry ldap_plugin do 
-  values "GroupAuthzRules" => authz_rules
-  type :multi_string
+registry_key ldap_plugin do 
+  values [ 
+    {
+      :name => "GroupAuthzRules",
+      :type => :multi_string,
+      :data => authz_rules
+    }
+  ]
   action :create
 end
 
@@ -139,21 +199,34 @@ add_groups_if = node[:pgina][:ldap][:add_to_groups_if]
 add_groups_if_not = node[:pgina][:ldap][:add_to_groups_not_if]
 rules = ldap_gateway_group_rules(always_groups, add_groups_if, add_groups_if_not)
 
-windows_registry ldap_plugin do 
-  values "GroupGatewayRules" => rules
-  type :multi_string
+
+registry_key ldap_plugin do 
+  values [ 
+    {
+      :name => "GroupGatewayRules",
+      :type => :multi_string,
+      :data => rules
+    }
+  ]
   action :create
 end
 
 # -------
 
 # Make sure LDAP is first otherwise users will not be able to login
-windows_registry pGina3 do
-  values(
-    "IPluginAuthenticationGateway_Order" => [ "0f52390b-c781-43ae-bd62-553c77fa4cf7","12fa152d-a2e3-4c8d-9535-5dcd49dfcb6d" ],
-    "IPluginAuthentication_Order"        => [ "0f52390b-c781-43ae-bd62-553c77fa4cf7"]
-  )
-  type :multi_string
+registry_key pGina3 do 
+  values [ 
+    {
+      :name => "IPluginAuthenticationGateway_Order",
+      :type => :multi_string,
+      :data => [ "0f52390b-c781-43ae-bd62-553c77fa4cf7","12fa152d-a2e3-4c8d-9535-5dcd49dfcb6d" ]
+    },
+    {
+      :name => "IPluginAuthentication_Order",
+      :type => :multi_string,
+      :data => [ "0f52390b-c781-43ae-bd62-553c77fa4cf7"]
+    }
+  ]
   only_if { node[:pgina][:ldap][:enabled] }
   action :create
 end
